d95db59e9501e55b795be18b48d5922a09d914a2
Ng Wing Tat, David <i@ngwingt.at>
2024-07-16 05:39:18 +0800

    🔧 Update base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/bodhisattva18/sample/',
     rootAttrs: {
       class: 'h-full min-h-svh',
     }
